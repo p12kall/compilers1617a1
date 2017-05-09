5c51556cba0fd11d4cfe0e3313d94bf30722bcb9
AndreasKalli <p12kall@users.noreply.github.com>
2017-05-09 14:18:14 +0300

    Update srt-fixer.py

--- srt-fixer.py
+++ srt-fixer.py
@@ -1,28 +1,59 @@
+# 1i Ergastiriaki Askisi - Metaglottistes (09.05.17)
+# ANDREAS KALLI
+# P2014186
+
+
 # import libraries
 import sys
 import re
 import argparse
 
 
+# Diatiposi tis morfis pou theloume na asxolithoume/epeksergastoume meso kanonikis ekfrasis
+# Morfi :	00:00:00.000 --> 00:00:00,000
+restrow = r'( \d{2} ) : ( \d{2} ) : ( \d{2} ) , ( \d{3} ) \s --> \s ( \d{2} ) : ( \d{2} ) : ( \d{2} ) , ( \d{3} )$'
+rexp = re.compile( restrow )
+
 parser = argparse.ArgumentParser()
 
 # add mandatory (positional) arguments
-parser.add_argument("fname",help="input srt file name")
-parser.add_argument("offset",type=float,help="subtitle offset in seconds to apply (can be fractional)")
+parser.add_argument( "fname", help = "input srt file name" )
+parser.add_argument( "offset", type = float, help = "subtitle offset in seconds to apply (can be fractional)" )
 
 # parse arguments
 args = parser.parse_args()
 
-with open(args.fname, newline='') as ifp:	
+
+# Anoigma tou arxeiou
+with open( args.fname, 'r', newline = '' ) as ifp:
+
+# Ksekiname tin sarosi tou keimenou ana grammi
+# kai entopismo ton grammon pou teriazoun stin
+# anazitisi mas
 	for line in ifp:
-	
-		# -- αντικαταστήστε με τον δικό σας κώδικα (αρχή) --
-		
-		
-		
-
+		m = rexp.search(line)
+		if m is not None:
 			
-	sys.stdout.write(line)
+# Gia ta deuterolepta tis arxis:
+# Pernoume to 3o group tis grammis
+			secBeginOld = m.group( 3 )
+# Metatrepoume se float kai prosthetoume to offset
+			secBeginNew = float( secBeginOld ) + args.offset
+# Metatrepoume pali se string
+			secBeginCorrect = str( secBeginNew )
+			convertBegin = re.sub( restrow, r'\3' + secBeginCorrect, line )
+			
+# Gia ta deuterolepta tou telous:
+# Pernoume to 7o group tis grammis
+			secEndOld = m.group( 7 )
+# Metatrepoume se float kai prosthetoume to offset
+			secEndNew = float( secEndOld ) + args.offset
+# Metatrepoume pali se string
+			secEndCorrect = str( secEndNew )
+			convertEnd = re.sub( restrow, r'\7' + secEndCorrect, line )
 
-		# -- αντικαταστήστε με τον δικό σας κώδικα (τέλος) --
+# Ektuposi tis ekastote grammis
+		sys.stdout.write(line)		
 
+# Kleisimo tou arxeiou .srt
+ifp.close()
